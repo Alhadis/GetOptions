42d537af4aa4b6b7db330db8364e83e4ea2ec872
Chris Sprance <chrissprance@gmail.com>
2018-10-09 23:28:14 -0400

    Added in Typescript Typings

---
+++ index.d.ts
@@ -0,0 +1,23 @@
+export declare type Options = { options: any; argv: string[] };
+export declare type Config = {
+	noAliasPropagation?: boolean;
+	noCamelCase?: boolean;
+	noBundling?: boolean;
+	ignoreEquals?: boolean;
+	duplicates?:
+		| 'use-first'
+		| 'use-last'
+		| 'limit-first'
+		| 'limit-last'
+		| 'error'
+		| 'append'
+		| 'stack'
+		| 'stack-values';
+};
+export declare function getOpts(
+	input: any[],
+	optdef: string | { [key: string]: string },
+	config: Config
+): Options;
+
+export default getOpts;
