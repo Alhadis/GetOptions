32e7c5bd640f595f6b60299d505fc69d94f3aa7a
Chris Sprance <chrissprance@gmail.com>
2018-10-10 14:39:21 -0400

    Merge upstream master

--- index.d.ts
+++ index.d.ts
@@ -1,18 +1,18 @@
 export declare type Options = { options: any; argv: string[] };
 export declare type Config = {
-	noAliasPropagation?: boolean | 'first-only';
+	noAliasPropagation?: boolean | "first-only";
 	noCamelCase?: boolean;
 	noBundling?: boolean;
 	ignoreEquals?: boolean;
 	duplicates?:
-		| 'use-first'
-		| 'use-last'
-		| 'limit-first'
-		| 'limit-last'
-		| 'error'
-		| 'append'
-		| 'stack'
-		| 'stack-values';
+		| "use-first"
+		| "use-last"
+		| "limit-first"
+		| "limit-last"
+		| "error"
+		| "append"
+		| "stack"
+		| "stack-values";
 };
 export declare function getOpts(
 	input: any[],
