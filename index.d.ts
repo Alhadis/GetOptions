9132740573f901d0d2e4a385ebd39b73c70cc5f9
Chris Sprance <chrissprance@gmail.com>
2018-10-13 04:04:50 -0400

    Add missing option to typings (#9)

--- index.d.ts
+++ index.d.ts
@@ -1,23 +1,34 @@
-export declare type Options = { options: any; argv: string[] };
-export declare type Config = {
-	noAliasPropagation?: boolean;
-	noCamelCase?: boolean;
-	noBundling?: boolean;
-	ignoreEquals?: boolean;
-	duplicates?:
-		| "use-first"
-		| "use-last"
-		| "limit-first"
-		| "limit-last"
-		| "error"
-		| "append"
-		| "stack"
-		| "stack-values";
-};
-export declare function getOpts(
-	input: any[],
-	optdef: string | { [key: string]: string },
-	config: Config
-): Options;
+// Import the function like 
+// import getOpts  = require('get-options');
+// If you need the types you can import them like normal
+// import { Options } from 'get-options';
 
-export default getOpts;
+/**
+ * Extract command-line options from a list of strings.
+ */
+declare function getOpts(
+	input: any[],
+	optdef?: string | { [key: string]: string },
+	config?: getOpts.Config
+): getOpts.Options;
+
+export = getOpts;
+
+declare namespace getOpts {
+	export type Options = { options: any; argv: string[] };
+	export type Config = {
+		noAliasPropagation?: boolean |  "first-only";
+		noCamelCase?: boolean;
+		noBundling?: boolean;
+		ignoreEquals?: boolean;
+		duplicates?:
+			| "use-first"
+			| "use-last"
+			| "limit-first"
+			| "limit-last"
+			| "error"
+			| "append"
+			| "stack"
+			| "stack-values";
+	};
+}
